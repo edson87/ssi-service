2e1b8ef8977eb8d108c708001f329e4c0975c299
Edson lopez <edson_lopez@hotmail.com.ar>
2018-07-03 11:13:19 -0400

    Initial setup

---
+++ src/main/java/com/dh/ssiservice/SsiServiceApplication.java
@@ -0,0 +1,12 @@
+package com.dh.ssiservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiServiceApplication.class, args);
+	}
+}
